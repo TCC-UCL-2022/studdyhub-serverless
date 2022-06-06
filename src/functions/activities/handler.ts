3b1fc24c0661e8894cfbb8b47e42ce254c8ef4ad
Felipe Ferrarini <ferrarinifelipe@gmail.com>
2022-06-05 22:39:07 -0300

    Add activityPosProcessing function

--- src/functions/activities/handler.ts
+++ src/functions/activities/handler.ts
@@ -1,3 +1,25 @@
+import { S3 } from "aws-sdk";
+import { S3Handler } from "aws-lambda";
 import { activityController } from "../../modules/factory";
+import { environments } from "../../config/environment";
 
 export const { createCourseActivity, getCourseActivities } = activityController;
+
+const s3Manager = new S3();
+
+export const activityPosProcessing: S3Handler = async (event) => {
+  for (const record of event.Records) {
+    try {
+      const bucket = record.s3.bucket.name;
+      const key = record.s3.object.key;
+      const params = {
+        Bucket: environments.S3_FRONTEND_UPLOAD_BUCKET,
+        Key: `protected/${key}`,
+        CopySource: `${bucket}/${key}`,
+      };
+      await s3Manager.copyObject(params).promise();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};

f15341a01ad30a7f14a581f5a6d8bdf32523c450
Crysthian Zanote Armini <52865098+crysthianzaar@users.noreply.github.com>
2022-07-02 19:10:51 -0300

    log

--- src/common/utils/generate-uuid.ts
+++ src/common/utils/generate-uuid.ts
@@ -1,2 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
-export const generateUuid = (): string => uuidv4();
+export const generateUuid = (): string => {
+    const uuid = uuidv4();
+    console.log("UUID GERADO: ", uuid);
+    return uuid;
+};
